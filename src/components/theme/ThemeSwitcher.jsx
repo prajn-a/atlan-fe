4f9b1e40ab3315a35f5926b3cc6ff2f3069c7bbb
Prajna <prajna11prabhu@gmail.com>
2024-01-05 09:30:55 +0530

    feat: theme toggle

---
+++ src/components/theme/ThemeSwitcher.jsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { useTheme } from "next-themes";
+import { SunDim, Moon } from "lucide-react";
+
+const ThemeSwitcher = () => {
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
+  return (
+    <button
+      onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+      className="p-1.5 rounded-md hover:dark:bg-zinc-800 hover:bg-gray-300 transition-all ease-out duration-100"
+    >
+      {theme === "dark" ? (
+        <SunDim size={20} strokeWidth={2} />
+      ) : (
+        <Moon size={17} strokeWidth={2} />
+      )}
+    </button>
+  );
+};
+
+export default ThemeSwitcher;
